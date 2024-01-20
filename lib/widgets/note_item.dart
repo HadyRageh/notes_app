0a18748e8055abee0beaefe890699fc07b11ec33
HadyRageh <seniorrageh@gmail.com>
2024-01-20 23:22:06 +0200

    create notes list view

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -7,48 +7,51 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 16, bottom: 16, left: 16),
-      decoration: BoxDecoration(
-        color: Colors.yellow,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              'Flutter Tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16.0, bottom: 16),
-              child: Text(
-                'Your time is limited; don\'t waste it living someone else\'s life.',
+    return Padding(
+      padding: const EdgeInsets.only(top: 16),
+      child: Container(
+        padding: EdgeInsets.only(top: 16, bottom: 16, left: 16),
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                'Flutter Tips',
                 style: TextStyle(
-                    color: Colors.black.withOpacity(0.5), fontSize: 15),
+                  color: Colors.black,
+                  fontSize: 26,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16.0, bottom: 16),
+                child: Text(
+                  'Your time is limited; don\'t waste it living someone else\'s life.',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.5), fontSize: 15),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                  size: 33,
+                ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.delete,
-                color: Colors.black,
-                size: 33,
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'may21,2024',
+                style: TextStyle(color: Colors.black.withOpacity(0.4)),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'may21,2024',
-              style: TextStyle(color: Colors.black.withOpacity(0.4)),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
